8451cc1398875135242e672e4096090a72874232
redspider110 <redspider110@163.com>
2017-08-24 16:45:23 +0800

    [mvp] design fun list presenter.

--- app/src/main/java/com/ymzs/funreading/presenter/BasePresenter.java
+++ app/src/main/java/com/ymzs/funreading/presenter/BasePresenter.java
@@ -1,8 +1,15 @@
 package com.ymzs.funreading.presenter;
 
+import com.ymzs.funreading.view.BaseView;
+
 /**
  * Created by xumingtao on 2017/8/22.
  */
 
-public interface BasePresenter {
+public interface BasePresenter<T extends BaseView> {
+    void subscribe();
+    void unSubscribe();
+
+    void takeView(T view);
+    void dropView();
 }
--- app/src/main/java/com/ymzs/funreading/presenter/FunListPresenter.java
+++ app/src/main/java/com/ymzs/funreading/presenter/FunListPresenter.java
@@ -1,8 +1,78 @@
 package com.ymzs.funreading.presenter;
 
+import com.ymzs.funreading.contract.FunListContract;
+import com.ymzs.funreading.model.DataRepository;
+import com.ymzs.funreading.model.DataSource;
+import com.ymzs.funreading.model.Fun;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 /**
  * Created by xumingtao on 2017/8/22.
  */
 
-public class FunListPresenter {
+public class FunListPresenter implements FunListContract.Presenter{
+    public DataRepository mDataRepository;
+    public FunListContract.View mFunListView;
+    public Disposable mDisposable;
+
+    public FunListPresenter(DataRepository dataRepository){
+        mDataRepository = dataRepository;
+    }
+
+    @Override
+    public void subscribe() {
+        loadFuns();
+    }
+
+    @Override
+    public void unSubscribe() {
+        dispose();
+    }
+
+    @Override
+    public void takeView(FunListContract.View view) {
+        mFunListView = view;
+    }
+
+    @Override
+    public void dropView() {
+        mFunListView = null;
+    }
+
+    private void loadFuns(){
+        mDataRepository.getFuns()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver<List<Fun>>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        mDisposable = d;
+                    }
+
+                    @Override
+                    public void onSuccess(@NonNull List<Fun> funs) {
+                        mFunListView.showFuns(funs);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        // null
+                    }
+                });
+
+    }
+
+    private void dispose(){
+        if(mDisposable != null && !mDisposable.isDisposed()){
+            mDisposable.dispose();
+        }
+    }
 }
