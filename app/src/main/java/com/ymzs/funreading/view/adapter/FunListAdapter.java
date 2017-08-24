c72e50fcc762e1ea4b4e41b418a2a840c19e99a5
redspider110 <redspider110@163.com>
2017-08-24 20:40:32 +0800

    [mvp][view] fill in the data for FunListFragment.

--- app/src/main/java/com/ymzs/funreading/view/adapter/FunListAdapter.java
+++ app/src/main/java/com/ymzs/funreading/view/adapter/FunListAdapter.java
@@ -1,8 +1,80 @@
 package com.ymzs.funreading.view.adapter;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.common.base.Strings;
+import com.ymzs.funreading.R;
+import com.ymzs.funreading.model.Fun;
+
+import java.util.List;
+
 /**
  * Created by xumingtao on 2017/8/22.
  */
 
-public class FunListAdapter {
+public class FunListAdapter extends RecyclerView.Adapter<FunListAdapter.FunListViewHolder> {
+    private Context mContext;
+    private List<Fun> mFunList;
+
+    public FunListAdapter(Context context) {
+        mContext = context;
+    }
+
+    public void setData(List<Fun> funs){
+        mFunList = funs;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public FunListAdapter.FunListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        FunListViewHolder holder = new FunListViewHolder(layoutInflater.inflate(R.layout.fun_list_item, parent, false));
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(FunListAdapter.FunListViewHolder holder, int position) {
+        Fun fun = mFunList.get(position);
+        String author = fun.getAuthor();
+        if(Strings.isNullOrEmpty(author)){
+            author = mContext.getString(R.string.unknown_author);
+        }
+        holder.mTvAuthor.setText(author);
+        holder.mTvTime.setText(String.valueOf(fun.getTime()));
+        holder.mTvContent.setText(fun.getContent());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFunList.size();
+    }
+
+    class FunListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+
+        TextView mTvAuthor;
+        TextView mTvTime;
+        TextView mTvContent;
+
+        public FunListViewHolder(View itemView) {
+            super(itemView);
+            mTvAuthor = (TextView) itemView.findViewById(R.id.fun_list_item_tv_author);
+            mTvTime = (TextView) itemView.findViewById(R.id.fun_list_item_tv_time);
+            mTvContent = (TextView) itemView.findViewById(R.id.fun_list_item_tv_content);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            return false;
+        }
+    }
 }
--- app/src/main/java/com/ymzs/funreading/view/fragment/FunListFragment.java
+++ app/src/main/java/com/ymzs/funreading/view/fragment/FunListFragment.java
@@ -1,9 +1,9 @@
 package com.ymzs.funreading.view.fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,44 +11,61 @@ import android.view.ViewGroup;
 import com.ymzs.funreading.R;
 import com.ymzs.funreading.contract.FunListContract;
 import com.ymzs.funreading.model.Fun;
-import com.ymzs.funreading.presenter.FunListPresenter;
+import com.ymzs.funreading.view.adapter.FunListAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public abstract class FunListFragment extends BaseFragment implements FunListContract.View{
-
     public FunListContract.Presenter mFunListPresenter;
+    private RecyclerView mFunRecyclerView;
+    private FunListAdapter mFunListAdapter;
 
     public FunListFragment() {
         // Required empty public constructor
     }
 
+    public void setFunListPresenter(FunListContract.Presenter presenter) {
+        mFunListPresenter = presenter;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_fun_list, container, false);
+        mFunRecyclerView = (RecyclerView) root.findViewById(R.id.fragment_fun_list_recyclerView);
+        mFunRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mFunListAdapter = new FunListAdapter(getContext());
+        mFunListAdapter.setData(new ArrayList<Fun>(0));
+        mFunRecyclerView.setAdapter(mFunListAdapter);
         return root;
     }
 
     @Override
     public void showFuns(List<Fun> funs) {
-
+        mFunListAdapter.setData(funs);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        //mFunListPresenter.takeView(this);
+        mFunListPresenter.takeView(this);
+        mFunListPresenter.subscribe();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mFunListPresenter.unSubscribe();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        //mFunListPresenter.dropView();
+        mFunListPresenter.dropView();
     }
 }
