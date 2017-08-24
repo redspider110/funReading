8ccd7754e8b3e8af119e903370ada20071d74a7a
redspider110 <redspider110@163.com>
2017-08-24 20:21:35 +0800

    [bug] retrofit: baseUrl must end in '/'
        llegalArgumentException: baseUrl must end in /: url

--- app/src/main/java/com/ymzs/funreading/model/remote/ApiConstants.java
+++ app/src/main/java/com/ymzs/funreading/model/remote/ApiConstants.java
@@ -5,7 +5,7 @@ package com.ymzs.funreading.model.remote;
  */
 
 public final class ApiConstants {
-    public static final String QSBK_TEXT_API_HOST = "http://m2.qiushibaike.com/article/list";
+    public static final String QSBK_TEXT_API_HOST = "http://m2.qiushibaike.com/article/list/";
     public static final String JIANDAN_JOKE_API_HOST = "http://i.jandan.net/?oxwlxojflwblxbsapi=jandan.get_duan_comments&page=%s";
     public static final String NHDZ_DUANZI_API_HOST = "http://iu.snssdk.com/neihan/stream/mix/v1/?content_type=-102&count=%s&min_time=%s";
 }
