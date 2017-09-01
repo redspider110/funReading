979868382431b5ac563b9ab1c24ab6162f47d269
redspider110 <redspider110@163.com>
2017-09-01 16:07:30 +0800

    [mvp][model][local][greendao]annotate Fun entity and init Dao.

--- app/src/main/java/com/ymzs/funreading/model/Fun.java
+++ app/src/main/java/com/ymzs/funreading/model/Fun.java
@@ -1,44 +1,83 @@
 package com.ymzs.funreading.model;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
 /**
  * Created by xumingtao on 2017/8/23.
  */
 
+@Entity
 public class Fun {
-    private String mAuthor;
-    private String mTime;
-    private String mContent;
+    @Id
+    private Long id;
+    private String author;
+    private String time;
+    private String content;
+    private int type;
 
-    public String getAuthor() {
-        return mAuthor;
+    @Generated(hash = 146934633)
+    public Fun(Long id, String author, String time, String content, int type) {
+        this.id = id;
+        this.author = author;
+        this.time = time;
+        this.content = content;
+        this.type = type;
     }
 
-    public void setAuthor(String mAuthor) {
-        this.mAuthor = mAuthor;
+    @Generated(hash = 1499424344)
+    public Fun() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public String getTime() {
-        return mTime;
+        return this.time;
     }
 
-    public void setTime(String mTime) {
-        this.mTime = mTime;
+    public void setTime(String time) {
+        this.time = time;
     }
 
     public String getContent() {
-        return mContent;
+        return this.content;
     }
 
-    public void setContent(String mContent) {
-        this.mContent = mContent;
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getType() {
+        return this.type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
     }
 
     @Override
     public String toString() {
         return "Fun{" +
-                "mAuthor='" + mAuthor + '\'' +
-                ", mTime=" + mTime +
-                ", mContent='" + mContent + '\'' +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", time='" + time + '\'' +
+                ", content='" + content + '\'' +
+                ", type=" + type +
                 '}';
     }
 }
--- app/src/main/java/com/ymzs/funreading/model/local/db/DbHelper.java
+++ app/src/main/java/com/ymzs/funreading/model/local/db/DbHelper.java
@@ -1,30 +1,35 @@
 package com.ymzs.funreading.model.local.db;
 
+
 import android.content.Context;
-import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import org.greenrobot.greendao.database.Database;
 
 /**
  * Created by xumingtao on 2017/8/22.
  */
 
-public class DbHelper extends SQLiteOpenHelper{
+public class DbHelper extends DaoMaster.OpenHelper{
 
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "FunReading.db";
+    private static final String DATABASE_NAME = "FunReading.db";
+    private static final boolean ENCRYPTED = false;
+    private DaoSession mDaoSession;
 
-    public DbHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-
+    public DbHelper(Context context){
+        super(context, DATABASE_NAME);
+        Database db = ENCRYPTED ? getEncryptedWritableDb("super-secret") : getWritableDb();
+        mDaoSession = new DaoMaster(db).newSession();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        super.onUpgrade(db, oldVersion, newVersion);
+        // upgrade db.
+        // ...
     }
+
+    public DaoSession getDaoSession(){
+        return mDaoSession;
+    }
+
 }
