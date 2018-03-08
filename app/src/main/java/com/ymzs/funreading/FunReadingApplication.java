767523eb6ab40dfa63936cbc61d0379191aaa8cf
redspider110 <redspider110@163.com>
2018-03-08 18:19:02 +0800

    [LeakCanary] add Leak Canary.

--- app/src/main/java/com/ymzs/funreading/FunReadingApplication.java
+++ app/src/main/java/com/ymzs/funreading/FunReadingApplication.java
@@ -1,6 +1,8 @@
 package com.ymzs.funreading;
 
 import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
 import com.ymzs.funreading.di.DaggerAppComponent;
 import com.ymzs.funreading.model.DataRepository;
 import javax.inject.Inject;
@@ -16,6 +18,14 @@ public class FunReadingApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        // leak canary
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+
         dataRepository = DaggerAppComponent.builder()
                 .providerContext(getApplicationContext())
                 .build()
